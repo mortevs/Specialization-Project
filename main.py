f1a6dc6d2e775e1d5b2ef747ade79e77dec7523a
mortevs <86227643+mortevs@users.noreply.github.com>
2024-06-06 13:36:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 if __name__ == "__main__":
     from GUI.GUI_class import GUI
     import streamlit as st
-    st.title("This is an old version of Smipps, that was developed in the specialization project")
-
-    st.set_page_config(
-        page_title="Old-Smipps",
-        page_icon="🖩",
-        layout="wide"
-     )
-    
+    st.title("This is an old version of Smipps, that was developed in the specialization project")    
     GUI()
 
 
